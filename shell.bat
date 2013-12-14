fa45c56663027438dc5c5c6affed96269ab01c5f
Sébastien HEUZE <heuze.seb@gmail.com>
2013-12-14 20:28:19 +0100

    Maquettes

---
+++ shell.bat
@@ -0,0 +1,12 @@
+Echo off
+Echo ***********************************
+Echo * Modification d'un utilisateur   *
+Echo ***********************************
+Echo identifiant de l'utilisateur a modifier ?
+Echo cdupont
+Echo Nom de l'utilisateur
+Echo.
+Echo Prenom de l'utilisateur
+Echo.
+Echo Droits administrateur
+Echo Oui
